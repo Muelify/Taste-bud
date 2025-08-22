e912f99e0f140e549e844690c7fc81badc162946
Muelify <samuelchidera11@gmail.com>
2025-08-22 15:00:08 +0100

    first comit

---
+++ rest.js
@@ -0,0 +1,7 @@
+'strict'
+document.addEventListener("shown.bs.offcanvas", function () {
+  document.body.style.paddingRight = "0";
+});
+document.addEventListener("hidden.bs.offcanvas", function () {
+  document.body.style.paddingRight = "0";
+});
